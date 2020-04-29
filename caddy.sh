32edb69c2fe8c7b5ccfdb9b73bf5ee8a02b5e58e
Jayson <cjszxzx520@163.com>
2020-04-29 18:58:34 +0800

    update

--- caddy.sh
+++ caddy.sh
@@ -16,5 +16,17 @@ proxy / $address
 
 ulimit -n 8192
 
+if ! wget --no-check-certificate https://raw.githubusercontent.com/cjs520/webbackup/master/manager/caddy -O /etc/init.d/caddy; then
+			echo -e " Caddy服务 管理脚本下载失败 ! 下载备用脚本" && wget --no-check-certificate https://gitee.com/jayson0201/webbackup/raw/master/manager/caddy -O /etc/init.d/caddy
+fi
+chmod +x /etc/init.d/caddy
+chkconfig --add caddy
+chkconfig caddy on
 
-echo "caddy 启动完毕"
+
+
+echo && echo -e " Caddy 使用命令：${caddy_conf_file}
+ 日志文件：cat /tmp/caddy.log
+ 使用说明：service caddy start | stop | restart | status
+ 或者使用：/etc/init.d/caddy start | stop | restart | status
+  Caddy 安装完成！" && echo
