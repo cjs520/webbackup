39ffcb7c62f1e533dfc6cebca09376addf358565
Jayson <cjszxzx520@163.com>
2021-07-14 12:29:06 +0800

    update

--- cloudenv.sh
+++ cloudenv.sh
@@ -15,9 +15,11 @@ elif [ "$tmp" == "2" ];then
   sleep 5
   echo "安装node，npm，yarn"
   curl -sL https://deb.nodesource.com/setup_13.x | sudo bash -
-  apt install -t nodejs
+  sudo apt-get install -y nodejs
   npm install -g n
   n latest
+ rm -rf /usr/bin/node
+ln -s /usr/local/bin/node /usr/bin/node
  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
   sudo apt install -y yarn
